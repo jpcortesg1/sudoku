7a8d856389a7baf95a01232935b7504cd7671f5a
jpcortesg <jpcortesg@eafit.edu.co>
2023-09-19 21:14:33 -0500

    Finish process of create notes in sudoku

--- src/components/Cell/NotesCell.tsx
+++ src/components/Cell/NotesCell.tsx
@@ -1,5 +1,4 @@
 import { setCurrentPanel } from "@/features/configuration/sudokuSlice";
-import useGetConfiguration from "@/hooks/redux/useGetConfiguration";
 import { RootState } from "@/store";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -19,8 +18,9 @@ export default function NotesCell(props: NotesCellProps) {
       active: false,
     }))
   );
+  const [iscorrect, setIsCorrect] = useState<boolean>(false);
 
-  const { isEditMode, currentCell, currentPanel } = useSelector(
+  const { isEditMode, currentCell, currentPanel, emptySudoku } = useSelector(
     (state: RootState) => state.sudoku
   );
   const dispatch = useDispatch();
@@ -39,19 +39,52 @@ export default function NotesCell(props: NotesCellProps) {
     dispatch(setCurrentPanel(null));
   }, [currentCell, col, row, currentPanel, dispatch]);
 
+  useEffect(() => {
+    if (!emptySudoku) return;
+    if (!emptySudoku[row][col]) {
+      setIsCorrect(false);
+      return;
+    }
+    setIsCorrect(true);
+  }, [emptySudoku, row, col]);
+
+  // Check each time the currentCell changes, to set acvtive notes
+  useEffect(() => {
+    const rowStart = Math.floor(Number(row) / 3) * 3;
+    const colStart = Math.floor(Number(col) / 3) * 3;
+    const rows = [rowStart, rowStart + 1, rowStart + 2];
+    const cols = [colStart, colStart + 1, colStart + 2];
+    const isRow = rows.includes(currentCell?.row);
+    const isCol = cols.includes(currentCell?.col);
+    const selected = emptySudoku[currentCell?.row][currentCell?.col];
+    if (
+      (currentCell?.row === row ||
+        currentCell?.col === col ||
+        (isRow && isCol)) &&
+      selected !== null
+    ) {
+      setNotes((prev) => [
+        ...prev.map((note) => ({
+          ...note,
+          active: note.value === selected ? false : note.active,
+        })),
+      ]);
+    }
+  }, [currentCell, col, row, emptySudoku]);
+
   return (
     <div
       className="absolute left-0 top-0 bg-slate-300 w-full h-full border-b-2 border-r-2 grid-rows-3 grid-cols-3 grid text-xs justify-items-center p-1"
       style={{
         ...style,
         cursor: "pointer",
-        zIndex: isEditMode ? 2 : -1,
+        zIndex: isEditMode && !iscorrect ? 2 : -1,
       }}
     >
       {notes.map((note) => (
         <span
           style={{
-            visibility: isEditMode && note.active ? "visible" : "hidden",
+            visibility: !iscorrect && note.active ? "visible" : "hidden",
             cursor: "pointer",
           }}
           key={note.value}
