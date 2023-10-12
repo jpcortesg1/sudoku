fbb62360bbc1af7e31757a38da153321f1fb2d6f
Juan Pablo Cortes <jpcortesg@eafit.edu.co>
2023-10-12 11:06:52 -0500

    Remove build

--- .next/static/DyNe9TgT-LJuENlL6AkFY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-1c6d6ff44bb0b80b.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
